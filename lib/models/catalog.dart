a5b7dc223f8be14bcf3413f1db53371821ae69db
Tarun Nayak <tarunnayak01122@gmail.com>
2023-03-21 16:39:35 +0530

    day 11 complete

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,19 @@
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 1,
+      name: "iPhone 14 Pro",
+      desc: "Apple iPhone 14th Generation",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209_GEO_EMEA?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1660753617539",
+    )
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -14,15 +28,3 @@ class Item {
       required this.color,
       required this.image});
 }
-
-final products = [
-  Item(
-    id: "Tarun",
-    name: "iPhone 14 Pro",
-    desc: "Apple iPhone 14th Generation",
-    price: 999,
-    color: "#33505a",
-    image:
-        "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209_GEO_EMEA?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1660753617539",
-  )
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
@@ -16,10 +18,15 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of Flutter by $name"),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: CatalogModel.items[index],
+              );
+            }),
       ),
       drawer: MyDrawer(),
     );
