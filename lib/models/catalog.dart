574286164f27ea1a51a8ed882ebd6c11dd254174
Tarun Nayak <tarunnayak01122@gmail.com>
2023-03-21 14:59:33 +0530

    day 9 complete

---
+++ lib/models/catalog.dart
@@ -0,0 +1,28 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
+
+final products = [
+  Item(
+    id: "Tarun",
+    name: "iPhone 14 Pro",
+    desc: "Apple iPhone 14th Generation",
+    price: 999,
+    color: "#33505a",
+    image:
+        "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209_GEO_EMEA?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1660753617539",
+  )
+];
